9c25f79daf683f99038544ee70f9597fe882bb28
Stan <cyclinglinguist@gmail.com>
2023-10-29 16:58:37 -0500

    feat: add test sprite

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,74 @@
 import './style.css'
-import { Scene, Game, WEBGL, GameObjects } from 'phaser';
+import { Scene, Game, WEBGL, GameObjects, Physics } from 'phaser'
+const WIDTH = 800
+const HEIGHT = 600
 
-const canvas = document.getElementById('game') as HTMLCanvasElement;
+
+const canvas = document.getElementById('game') as HTMLCanvasElement
 
 class GameScene extends Scene {
-  private textbox: GameObjects.Text | undefined;
-
+  #textbox: GameObjects.Text | undefined
+  #thing: Physics.Arcade.Sprite | undefined
   constructor() {
-    super('scene-game');
+    super('scene-game')
+    
+  }
+
+  
+
+  preload() {
+    
+    this.load.image('kiwi', '/kiwi.png');
+    this.load.svg('pause', 'https://assets.codepen.io/4261124/pause.svg');
   }
 
   create() {
-    this.textbox = this.add.text(
-      window.innerWidth / 2,
-      window.innerHeight / 2,
+    this.physics.world.setBounds(0,0, WIDTH, HEIGHT)
+    
+
+
+    this.#textbox = this.add.text(
+      WIDTH / 2,
+      HEIGHT / 2,
       'Welcome to Phaser x Vite!',
       {
         color: '#FFF',
         fontFamily: 'monospace',
-        fontSize: '26px'
+        fontSize: '26px',
       }
-    );
+    )
 
-    this.textbox.setOrigin(0.5, 0.5);
+    this.#thing = this.physics.add.sprite(
+      10, 10, "kiwi"
+    )
+    this.#thing.setCollideWorldBounds()
+
+    this.#textbox.setOrigin(0.5, 0.5)
   }
 
   update(time: number, delta: number) {
-    if (!this.textbox) {
-      return;
+    if (!this.#textbox) {
+      return
     }
 
-    this.textbox.rotation += 0.0005 * delta;
+    this.#textbox.rotation += 0.0005 * delta
   }
 }
 
 const config = {
   type: WEBGL,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: WIDTH,
+  height: HEIGHT,
   canvas,
   physics: {
     default: 'arcade',
     arcade: {
       // pixels per second
-      gravity: { y: -100 },
+      gravity: { y: 25 },
       // debug: true
-    }
+    },
   },
-  scene: [
-    GameScene
-  ]
+  scene: [GameScene],
 }
 
-new Game(config);
+new Game(config)
