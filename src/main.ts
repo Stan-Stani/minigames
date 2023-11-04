536b7cd358ea109ad8dd8af1a030f919642fc569
Stan <cyclinglinguist@gmail.com>
2023-11-03 22:24:19 -0500

    feat: implement basic controls and friction

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ const HEIGHT = 240
 const canvas = document.getElementById('game') as HTMLCanvasElement
 
 class GameScene extends Scene {
-  #textbox: GameObjects.Text | undefined
-  #thing: Physics.Arcade.Sprite | undefined
+  #textbox: GameObjects.Text
+  #playerOne?: Physics.Arcade.Sprite
   constructor() {
     super('scene-game')
   }
+  isRunning = false
 
   preload() {
     this.load.image('kiwi', '/kiwi.png')
@@ -31,8 +32,8 @@ class GameScene extends Scene {
       }
     )
 
-    this.#thing = this.physics.add.sprite(10, 10, 'kiwi')
-    this.#thing.setCollideWorldBounds()
+    this.#playerOne = this.physics.add.sprite(10, 10, 'kiwi')
+    this.#playerOne.setCollideWorldBounds()
 
     this.#textbox.setOrigin(0.5, 0.5)
 
@@ -45,12 +46,35 @@ class GameScene extends Scene {
         var spaceBar = this.input.keyboard.addKey(
           Phaser.Input.Keyboard.KeyCodes.SPACE
         )
+        var right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        var left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
 
-        // spaceBar.on('down', () => {
-        //   logMessage('down')
-        // })
+        spaceBar.on('down', () => {
+          toastMessage(this.#playerOne)
+          this.#playerOne?.setVelocityY(-30)
+        })
+
+        right
+          .on('down', () => {
+            this.#playerOne?.setAccelerationX(30)
+            this.isRunning = true
+          })
+          .on('up', () => {
+            this.#playerOne?.setAccelerationX(0)
+            this.isRunning = false
+          })
+
+        left
+          .on('down', () => {
+            this.#playerOne?.setAccelerationX(-30)
+            this.isRunning = true
+          })
+          .on('up', () => {
+            this.#playerOne?.setAccelerationX(0)
+            this.isRunning = false
+          })
       } catch (e: any) {
-        logMessage(e.message)
+        toastMessage(e.message)
       }
     }
 
@@ -63,10 +87,61 @@ class GameScene extends Scene {
     }
 
     this.#textbox.rotation += 0.0005 * delta
+    stickyMessage(this.#playerOne?.body?.velocity)
+
+    const friction = 0.35 // friction coefficient
+    const deltaInSeconds = delta / 1000 // Convert delta to seconds
+
+    // Apply friction factor to the player's velocity and make it frame rate independent
+
+    if (!this.isRunning && this.#playerOne?.body?.velocity.x) {
+      this.#playerOne.body.velocity.x *= Math.pow(friction, deltaInSeconds)
+
+      // Stop the sprite if the velocity is very low
+      if (Math.abs(this.#playerOne.body.velocity.x) < 0.1) {
+        this.#playerOne.body.velocity.x = 0
+      }
+    }
   }
 }
 
-const config = {
+function stickyMessage(message: any) {
+  console.log(message)
+  const prettyMessage =
+    typeof message === 'string' || typeof message === 'number'
+      ? String(message)
+      : JSON.stringify(message)
+
+  if (!document.getElementById('sticky-message')) {
+    const logDiv = document.getElementById('log')
+    const messageDiv = document.createElement('div')
+    messageDiv.id = 'sticky-message'
+    messageDiv.classList.add('log-message', 'fade-in')
+    logDiv?.appendChild(messageDiv)
+  }
+
+  const stickyMessage = document.getElementById('sticky-message')!
+  stickyMessage.textContent = prettyMessage
+}
+
+function toastMessage(message: any) {
+  console.log(message)
+  const prettyMessage =
+    typeof message === 'string' || typeof message === 'number'
+      ? String(message)
+      : JSON.stringify(message)
+  const logDiv = document.getElementById('log')
+  const messageDiv = document.createElement('div')
+  messageDiv.classList.add('log-message', 'fade-in')
+  logDiv?.appendChild(messageDiv)
+  messageDiv?.append(prettyMessage)
+  setTimeout(() => {
+    messageDiv.classList.add('fade-out')
+    setTimeout(() => messageDiv.remove(), 2000)
+  }, 7000)
+}
+
+const config: Phaser.Types.Core.GameConfig = {
   type: WEBGL,
   width: WIDTH,
   height: HEIGHT,
@@ -82,23 +157,10 @@ const config = {
   scene: [GameScene],
   pixelArt: true,
   scale: {
-    mode: Phaser.Scale.FIT,
+    parent: 'game-wrapper',
+    // mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-}
-
-function logMessage(message: any) {
-  console.log(message)
-  const logDiv = document.getElementById('log')
-  const messageDiv = document.createElement('div')
-  messageDiv.classList.add('log-message', 'fade-in')
-  const logMessageDiv = logDiv?.appendChild(messageDiv)
-  logMessageDiv?.append(JSON.stringify(message))
-  setTimeout(() => {
-    messageDiv.classList.add('fade-out')
-    setTimeout(() => messageDiv.remove(), 2000)
-  }, 7000)
-  
 }
 
 new Game(config)
